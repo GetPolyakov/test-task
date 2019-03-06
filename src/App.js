5b9100d2bcc69ff50fb72c64f9179f9cb189ac85
Sergey Polyakov <polyakov@Admins-MacBook-Air.local>
2019-03-06 22:42:25 +0300

    Setup dependencies

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { TEST } from "./actions/testAction";
 import './App.css';
+import Test from "./Test";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Switch>
+            <Route path="/" exact render={(props) => <div>test</div>} />
+            <Route path="/omg" component={Test} />
+        </Switch>
     );
   }
 }
+
 
 export default App;
